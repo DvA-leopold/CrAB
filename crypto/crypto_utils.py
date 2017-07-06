a5588ea03d015e400f747faedc848e8044850168
Vlad Durnev <leopold.dva@gmail.com>
2017-07-06 00:16:52 +0000

    Refactoring and minor fixes in crypto module

--- crypto/crypto_utils.py
+++ crypto/crypto_utils.py
@@ -13,20 +13,24 @@ class CurveCrypto:
     def __init__(self):
         self.backend = Backend()
         self.curve = SECP256K1()
-        self.private_key = None # FIXME should i keep this key in memory?
+        self.private_key = None  # FIXME should i keep this key in memory?
 
-    def sign_data(self, data: bytes):
+    def sign_data(self, data: bytes) -> bytes:
         if not self.private_key:
             self.private_key = generate_private_key(self.curve, self.backend)
-        signature = self.private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+        signature = self.private_key.sign(data, ec.ECDSA(hashes.SHA256()))  # TODO bitcoin use double sha
         return signature
 
-    def verify_data(self, signature, data: bytes):
+    def verify_data(self, signature: bytes, data: bytes) -> bool:
         try:
             self.private_key.public_key().verify(signature, data, ec.ECDSA(hashes.SHA256()))
             return True
         except InvalidSignature:
             return False
+
+    def __dump_private(self):
+        # TODO dump key to a file
+        pass
 
     def print_curve_info(self):
         print('curve name: ', self.curve.name, '\nkey size: ', self.curve.key_size)

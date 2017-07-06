726da7f1da146e52c317581511a89d2a698047ee
Vlad Durnev <leopold.dva@gmail.com>
2017-07-06 00:20:57 +0000

    Simplify main cos its useless for now

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-import shutil
-
-from storage.database import Database
-
 if __name__ == '__main__':
-    blockchain_database = Database('/home/leo/PycharmProjects/CrAB/db')
-
-    hash, header, data = blockchain_database.put_block(('vasl1', 'val2', 'val3', 'vqwe'))
-    print('hello workd')
-    print(hash)
-    print(blockchain_database.get_block(hash, True))
-
-    # shutil.rmtree('/home/leo/PycharmProjects/CrAB/db')
+    print('hello blockchain')

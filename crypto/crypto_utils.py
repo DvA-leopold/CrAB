3bf9dbb214a8dc9ad7f3795134bebfc024ab2354
Vlad Durnev <leopold.dva@gmail.com>
2017-08-04 01:04:54 +0300

    add crypto module and simple unit tests for it

--- crypto/crypto_utils.py
+++ crypto/crypto_utils.py
@@ -1,8 +1,9 @@
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends.openssl.backend import Backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric.ec import generate_private_key, SECP256K1
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
 '''
 This code must be carefully checked for crypto issues
@@ -13,24 +14,44 @@ class CurveCrypto:
     def __init__(self):
         self.backend = Backend()
         self.curve = SECP256K1()
-        self.private_key = None  # FIXME should i keep this key in memory?
+        self.private_key = None
+        self.public_key = None
+
+    def generate_private_key(self):
+        self.private_key = generate_private_key(self.curve, self.backend)
+        self.public_key = self.private_key.public_key()
 
     def sign_data(self, data: bytes) -> bytes:
-        if not self.private_key:
-            self.private_key = generate_private_key(self.curve, self.backend)
-        signature = self.private_key.sign(data, ec.ECDSA(hashes.SHA256()))  # TODO bitcoin use double sha
+        signature = self.private_key.sign(data, ec.ECDSA(hashes.SHA256()))  # bitcoin use double sha
         return signature
 
     def verify_data(self, signature: bytes, data: bytes) -> bool:
         try:
-            self.private_key.public_key().verify(signature, data, ec.ECDSA(hashes.SHA256()))
+            self.public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
             return True
         except InvalidSignature:
             return False
 
-    def __dump_private(self):
-        # TODO dump key to a file
-        pass
+    def dump_private_key(self, password: str, path_to_key: str):
+        with open(path_to_key, 'wb') as pem_file:
+            available_encryption = serialization.BestAvailableEncryption(password.encode())
+            private_key_bytes = self.private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                               format=serialization.PrivateFormat.PKCS8,
+                                                               encryption_algorithm=available_encryption)
+            pem_file.write(private_key_bytes)
 
-    def print_curve_info(self):
-        print('curve name: ', self.curve.name, '\nkey size: ', self.curve.key_size)
+    def load_private_key(self, password: str, path_to_key: str):
+        with open(path_to_key, 'rb') as pem_file:
+            bin_private_key = pem_file.read()
+            self.private_key = load_pem_private_key(bin_private_key, password.encode(), self.backend)
+
+    def dump_public_key(self, path_to_key: str):
+        with open(path_to_key, 'wb') as pem_file:
+            public_key_bytes = self.public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+            pem_file.write(public_key_bytes)
+
+    def load_public_key(self, path_to_key: str):
+        with open(path_to_key, 'rb') as pem_file:
+            bin_public_key = pem_file.read()
+            self.public_key = load_pem_public_key(bin_public_key, self.backend)
--- tests/crypto_tests.py
+++ tests/crypto_tests.py
@@ -1,42 +1,62 @@
+import shutil
 import unittest
+
+from cryptography.hazmat.primitives import serialization
 
 from crypto.crypto_utils import CurveCrypto
 
 
 class CryptoTests(unittest.TestCase):
+    def setUp(self):
+        self.crypto = CurveCrypto()
+        self.crypto.generate_private_key()
+
     def test_signing_same_data(self):
-        crypto = CurveCrypto()
         data = 'data for tests'.encode()
-        signature = crypto.sign_data(data)
-        self.assertTrue(crypto.verify_data(signature, data),
+        signature = self.crypto.sign_data(data)
+        self.assertTrue(self.crypto.verify_data(signature, data),
                         'signature not match')
 
     def test_signing_not_same_data(self):
-        crypto = CurveCrypto()
         data1 = 'data for tests'.encode()
         data2 = 'data for test1'.encode()
-        signature = crypto.sign_data(data1)
-        self.assertFalse(crypto.verify_data(signature, data2),
+        signature = self.crypto.sign_data(data1)
+        self.assertFalse(self.crypto.verify_data(signature, data2),
                          'signature match, but should not cos data is different')
-        self.assertTrue(crypto.verify_data(signature, data1),
+        self.assertTrue(self.crypto.verify_data(signature, data1),
                         'signature not mutch, but data was sign with this signature')
 
     def test_sign_empty_data(self):
-        crypto = CurveCrypto()
         data = ''.encode()
-        signature = crypto.sign_data(data)
-        self.assertTrue(crypto.verify_data(signature, data),
+        signature = self.crypto.sign_data(data)
+        self.assertTrue(self.crypto.verify_data(signature, data),
                         'empty data signature do not match')
 
     def test_sign_several_data_chunks(self):
-        crypto = CurveCrypto()
         data1 = 'data1 for test'.encode()
-        signature1 = crypto.sign_data(data1)
+        signature1 = self.crypto.sign_data(data1)
         data2 = 'data2 for test2'.encode()
-        signature2 = crypto.sign_data(data2)
-        self.assertTrue(crypto.verify_data(signature1, data1),
+        signature2 = self.crypto.sign_data(data2)
+        self.assertTrue(self.crypto.verify_data(signature1, data1),
                         'signature for data1 not match')
-        self.assertTrue(crypto.verify_data(signature2, data2),
+        self.assertTrue(self.crypto.verify_data(signature2, data2),
                         'signature for data2 not match')
-        self.assertFalse(crypto.verify_data(signature1, data2),
+        self.assertFalse(self.crypto.verify_data(signature1, data2),
                          'signature1 for data2 match, but should not')
+
+    def test_dump_private_key(self):
+        available_encryption = serialization.BestAvailableEncryption('password'.encode())
+        self.crypto.dump_private_key('password', '/tmp/crab.pem')
+        key_before_dump = self.crypto.private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                                format=serialization.PrivateFormat.PKCS8,
+                                                                encryption_algorithm=available_encryption)
+        self.crypto.load_private_key('password', '/tmp/crab.pem')
+        key_after_dump = self.crypto.private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                                format=serialization.PrivateFormat.PKCS8,
+                                                                encryption_algorithm=available_encryption)
+        self.assertNotEqual(key_before_dump, key_after_dump, 'two keys must be indentical, but they are not')
+
+    def test_wrong_password_dump_key(self):
+        self.crypto.dump_private_key('password', '/tmp/crab.pem')
+        with self.assertRaises(ValueError):
+            self.crypto.load_private_key('password_wrong', '/tmp/crab.pem')
